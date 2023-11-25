a9069fe639c9ef0bcee4843387a01a7dcbce795f
chloe-bowers <chlo_bo@live.com.au>
2023-11-25 12:17:49 +0100

    chore: files changed with changes in: useFetchHouses.tsx

---
+++ src/hooks/useFetchHouses.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+interface Member {
+  name: string;
+  slug: string;
+}
+
+interface House {
+  slug: string;
+  name: string;
+  members: Member[];
+}
+
+const useFetchHouses = () => {
+  const [houses, setHouses] = useState<House[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.gameofthronesquotes.xyz/v1/houses'
+        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch houses');
+        }
+
+        const data: House[] = await response.json();
+        setHouses(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error as Error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { houses, loading, error };
+};
+
+export default useFetchHouses;
